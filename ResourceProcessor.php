bca7b3eb04a355014928d5e5ed188b3bc4ea8ebc
Wolfy-J <wolfy.jd@gmail.com>
2015-04-05 18:49:36 +0300

    Markdown moved to toolkit.

--- ResourceProcessor.php
+++ ResourceProcessor.php
@@ -8,34 +8,35 @@
  */
 namespace Spiral\Toolkit;
 
+use Spiral\Components\View\LayeredCompiler;
 use Spiral\Components\View\ProcessorInterface;
-use Spiral\Components\View\ViewManager;
 
 class ResourceProcessor implements ProcessorInterface
 {
     /**
      * New processors instance with options specified in view config.
      *
-     * @param array       $options
-     * @param ViewManager $view View component instance (if presented).
+     * @param LayeredCompiler $compiler Compiler instance.
+     * @param array           $options
      */
-    public function __construct(array $options, ViewManager $view = null)
+    public function __construct(LayeredCompiler $compiler, array $options)
     {
-        //Resource manager config should be stored separately
     }
 
     /**
-     * Performs view code pre-processing. View component will provide view source into processors,
+     * Performs view code pre-processing. LayeredCompiler will provide view source into processors,
      * processors can perform any source manipulations using this code expect final rendering.
      *
      * @param string $source    View source (code).
-     * @param string $view      View name.
      * @param string $namespace View namespace.
+     * @param string $view      View name.
+     * @param string $input     Input filename (usually real view file).
+     * @param string $output    Output filename (usually view cache, target file may not exists).
      * @return string
      */
-    public function processSource($source, $view, $namespace)
+    public function processSource($source, $namespace, $view, $input = '', $output = '')
     {
-        //Nothing for now
+        //Nothing to do for now
         return $source;
     }
 }
