0e9a34d1ebe8c4db5dc1a4b7b11dcc2d75d82656
Wolfy-J <wolfy.jd@gmail.com>
2015-07-20 20:51:41 +0300

    few updates

--- source/views/form.php
+++ source/views/form.php
@@ -1,6 +1,6 @@
 <asset:script href="/resources/scripts/bundle.js"/>
 <form action="${action}" method="${method|post}" enctype="multipart/form-data"
-      accept-charset="UTF-8" node:attributes="exclude:context"
+      accept-charset="UTF-8" node:attributes
       class="<?= (${ajax | true} ? 'js-spiral-form' : '') . ('${class}' ? ' ${class}' : '') #compiled  ?>">
     ${context}
 </form>
