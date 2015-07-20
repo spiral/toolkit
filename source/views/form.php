a31024bcedbb9b5fceb4493e8129bd79178354fc
Wolfy-J <wolfy.jd@gmail.com>
2015-07-20 20:50:55 +0300

    few updates

--- source/views/form.php
+++ source/views/form.php
@@ -1,29 +1,6 @@
 <asset:script href="/resources/scripts/bundle.js"/>
-
-<?php #compiled
-static $globalID;
-$formID = '${id}' ?: 'form-' . ( ++ $globalID );
-?>
-<form action="${action}" id="<?php #compiled
-echo $formID
-?>"
-      class="<?php #compiled
-      echo ( ${ajax | true} ? 'js-spiral-form' : '' ) . ( '${class}' ? ' ${class}' : '' )
-      ?>"
-      method="${method|post}" role="form" enctype="multipart/form-data" accept-charset="UTF-8" node:attributes>
-
-    <?php #compiled
-    ob_start();
-    ?>${labels.class}<?php #compiled
-    $labelsClass = ob_get_clean();
-
-    ob_start();
-    ?>${controls.class}<?php #compiled
-    $controlsClass = ob_get_clean();
-    ?>
-    <block:context/>
-    <?php #compiled
-    $labelsClass   = '';
-    $controlsClass = '';
-    ?>
+<form action="${action}" method="${method|post}" enctype="multipart/form-data"
+      accept-charset="UTF-8" node:attributes="exclude:context"
+      class="<?= (${ajax | true} ? 'js-spiral-form' : '') . ('${class}' ? ' ${class}' : '') #compiled  ?>">
+    ${context}
 </form>
