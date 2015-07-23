efc6a8c9e5a49725f1f8cbbbb1ca7cd490b2196a
Wolfy-J <wolfy.jd@gmail.com>
2015-07-23 23:03:08 +0300

    more inputs.

--- source/views/form.php
+++ source/views/form.php
@@ -7,7 +7,7 @@
 <block:form-class>${class} <?= (${ajax | true} ? 'js-spiral-form' : '') #compiled ?></block:form-class>
 
 <block:body>
-    <form action="${action}" method="${method|post}" enctype="multipart/form-data" accept-charset="UTF-8" class="${form-class}" node:attributes>
+    <form action="${action}" method="${method|post}" enctype="multipart/form-data" accept-charset="UTF-8" class="${form-class}" node:attributes="exclude:form-class">
         ${context}
     </form>
 </block:body>
