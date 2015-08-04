ca36f60aa823b52a542726625d0d81c5cee88320
Alex <alex@spiralscout.com>
2015-08-04 11:35:53 +0300

    editor tinymce

--- source/views/form/editor.php
+++ source/views/form/editor.php
@@ -1,21 +1,24 @@
-<extends:element/>
+<extends:form.input/>
 
 <block:assets>
     <asset:script href="/resources/scripts/spiral/bundle.js"/>
     <asset:script href="/resources/vendor/tinymce/tinymce.min.js"/>
     <script>
-        //todo editor id, to have multiple editors
         //todo rewrite to asset:inline script when it will work
-        //todo check what plugins we need by default
         //todo file browser
+        //todo autoId
         document.addEventListener("DOMContentLoaded", function () {
-            var height = document.getElementById("${id|editor}").offsetHeight;
             tinymce.init({
                 theme: "modern",
                 entity_encoding: "raw",
-                selector: "textarea#${id|editor}",
-                menubar: "edit insert view format table tools",
-//                plugins: "paste searchreplace charmap fullscreen link anchor image textcolor table media contextmenu",
+                selector: "#${id|editor-mce}",
+                plugins: [
+                    "lists link image charmap print preview anchor",
+                    "searchreplace code fullscreen",
+                    "insertdatetime media table contextmenu paste"
+                ],
+                toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | " +
+                "bullist numlist | link image",
                 convert_urls: false,
 //                file_browser_callback: spiral.fileBrowser.showModal,
                 //todo check this
@@ -32,14 +35,13 @@
                     ed.on("keyup change", function () {
                         this.save();
                     });
-                },
-                height: height - 109 + "px"//todo magic variable?
+                }
             })
         });
     </script>
 
 </block:assets>
 
-<block:body>
+<block:input-body>
     <textarea name="${name}" id="${id|editor}" node:attributes>${value}${context}</textarea>
-</block:body>
+</block:input-body>
