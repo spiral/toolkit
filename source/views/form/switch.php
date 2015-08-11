774d5578762990197cb6ffb52a32887bbe582a50
Nataliya Zhukava <nataliya.zhukava@gmail.com>
2015-08-12 00:30:31 +0300

    on/off switch

---
+++ source/views/form/switch.php
@@ -0,0 +1,11 @@
+<extends:element/>
+
+<block:body>
+    <div class="item-form-switch ${wrapper-class}">
+        <label class="item-label-switch">
+            <input type="checkbox" name="switch" class="switch-checkbox" checked node:attributes="exclude:context"/>
+            <span class="switch-inner"></span>
+            <span class="switch-switch"></span>
+        </label>
+    </div>
+</block:body>
