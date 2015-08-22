38e4528bc0e9fd533f8bea99efdd8d7c6fbfccbd
Wolfy-J <wolfy.jd@gmail.com>
2015-08-22 17:04:04 +0300

    switch fixed

--- source/views/form/switch.php
+++ source/views/form/switch.php
@@ -1,10 +1,18 @@
 <extends:element/>
 
 <block:body>
+    <?php #compile
+    //We need value set as php variable __values__
+    createVariable('__checked__', '${value}');
+    ?>
+    <?php
+    $__checked__ = !empty($__checked__) ? 'checked' : '';
+    ?>
     <div class="item-form-switch ${wrapper-class}">
         <label class="item-label-switch">
-            <input type="hidden" name="${name}" value="${default}"/>
-            <input type="checkbox" name="switch" class="switch-checkbox" node:attributes="exclude:context"/>
+            <input type="hidden" name="${name}" value="${no|0}"/>
+            <input type="checkbox" name="${name}" value="${yes|1}" class="switch-checkbox"
+                <?= $__checked__ ?> node:attributes="exclude:context,value"/>
             <span class="switch-inner"></span>
             <span class="switch-switch"></span>
         </label>
