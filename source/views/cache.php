7b73af3094a7db365f1a74ade2057fcc0f5ff7af
Wolfy-J <wolfy.jd@gmail.com>
2015-10-21 16:38:53 +0300

    cache element

---
+++ source/views/cache.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @var \Spiral\Cache\StoreInterface $__cacheStore__
+ */
+$__cacheStore__ = $this->container->get(\Spiral\Cache\StoreInterface::class);
+
+$__cacheKey__ = !empty('${key}') ? '${key}' : md5(__FILE__ . '.' . __LINE__);
+
+if ($__cacheStore__->has($__cacheKey__)) {
+    $__cached__ = $__cacheStore__->get($__cacheKey__);
+} else {
+    ob_start();
+    ?>${context}<?php
+    $__cacheStore__->set(
+        $__cached__ = ob_get_clean(),
+        $__cached__,
+        (int)'${lifetime|60}'
+    );
+}
+
+echo $__cached__;
+?>
