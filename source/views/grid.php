2191d6e9c3a6f59056c17c50407dc3a5c1d3a184
Wolfy-J <wolfy.jd@gmail.com>
2015-10-21 16:15:04 +0300

    Minor improvements for grid.

--- source/views/grid.php
+++ source/views/grid.php
@@ -1,40 +1,49 @@
-<?php #compile
-/**
- * Our source.
- */
-createVariable('__source__', '${source}');
+<extends:self:element/>
 
-/**
- * First of all we have to collect every defined cell element to built list of table headers
- * and elements.
- */
-
-$__gridHeaders__ = [];
-$__gridColumns__ = [];
-
-ob_start();
-?>${context}<?php #compile
-$columns = ob_get_clean();
-
-?>
-<table class="table ${class}">
-    <thead>
-    <tr>
-        <?php #compile
-        foreach ($__gridHeaders__ as $header) {
-            echo "<th><?=$header?></th>";
-        }
-        ?>
-    </tr>
-    </thead>
-    <tbody>
+<block:body>
     <?php #compile
-    echo '<?php foreach ($__source__ as $${as|item}) { ?>';
-    echo '<tr>';
-    echo join("\n", $__gridColumns__);
-    echo '</tr>';
-    echo '<?php } ?>';
+    /**
+     * Our source.
+     */
+    createVariable('__source__', '${source}');
+
+    /**
+     * First of all we have to collect every defined cell element to built list of table headers
+     * and elements.
+     */
+
+    $__gridHeaders__ = [];
+    $__gridColumns__ = [];
+
+    //Collecting headers and columns
+    ob_start();
+    ?>${context}<?php #compile
+    $columns = ob_get_clean();
+
     ?>
-    </tbody>
-</table>
-<spiral:paginator source="<?= $__source__ ?>"/>
+    <block:table>
+        <table class="table ${class}">
+            <thead>
+            <tr>
+                <?php #compile
+                foreach ($__gridHeaders__ as $header) {
+                    echo "<th><?=$header?></th>";
+                }
+                ?>
+            </tr>
+            </thead>
+            <tbody>
+            <?php #compile
+            echo '<?php foreach ($__source__ as $${as|item}) { ?>';
+            echo '<tr>';
+            echo join("\n", $__gridColumns__);
+            echo '</tr>';
+            echo '<?php } ?>';
+            ?>
+            </tbody>
+        </table>
+    </block:table>
+    <block:paginator>
+        <spiral:paginator source="<?= $__source__ ?>"/>
+    </block:paginator>
+</block:body>
