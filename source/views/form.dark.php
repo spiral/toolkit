efe20ea96d96ce1b9695d2f84fc2e4e5e06acdc8
Wolfy-J <wolfy.jd@gmail.com>
2015-12-14 23:26:17 +0300

    form improved

--- source/views/form.dark.php
+++ source/views/form.dark.php
@@ -9,14 +9,11 @@
 We can't set blur on form itself, because it will blur lockers(loaders) too.
 -->
 <block:body>
-    <form action="${action}" method="${method|post}" enctype="multipart/form-data" accept-charset="UTF-8" class="${form-class}" node:attributes="exclude:form-class">
+    <form action="${action}" method="${method|post}" enctype="multipart/form-data"
+          accept-charset="UTF-8" node:attributes="exclude:form-class"
+          class="${class} <?= (${ajax | true} ? 'js-sf-form' : '') #compiled ?>">
         <div class="form-content">
             <yield:context/>
         </div>
     </form>
 </block:body>
-
-<!--We are mofing form class at bottom so it can accept user defined ajax value.-->
-<block:form-class>
-    ${class} <?= (${ajax|true} ? 'js-sf-form' : '') #compiled ?>
-</block:form-class>
