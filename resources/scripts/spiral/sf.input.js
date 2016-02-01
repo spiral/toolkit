058da87646c7ccdb52f9af2f83f21f6632cf2d36
Nataliya Zhukava <nataliya.zhukava@gmail.com>
2016-02-01 23:48:43 +0300

    input prefix

---
+++ resources/scripts/spiral/sf.input.js
@@ -0,0 +1,233 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("sf"));
+	else if(typeof define === 'function' && define.amd)
+		define(["sf"], factory);
+	else {
+		var a = typeof exports === 'object' ? factory(require("sf")) : factory(root["sf"]);
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, function(__WEBPACK_EXTERNAL_MODULE_1__) {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	var _sf = __webpack_require__(1);
+	
+	var _sf2 = _interopRequireDefault(_sf);
+	
+	var _input = __webpack_require__(2);
+	
+	var _input2 = _interopRequireDefault(_input);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	_sf2.default.instancesController.registerInstanceType(_input2.default, "sf-js-input"); //resolved in webpack's "externals"
+	
+	module.exports = _input2.default; // ES6 default export will not expose us as global
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	module.exports = __WEBPACK_EXTERNAL_MODULE_1__;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	var _create = __webpack_require__(3);
+	
+	var _create2 = _interopRequireDefault(_create);
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	exports.default = undefined;
+	
+	var _sf = __webpack_require__(1);
+	
+	var _sf2 = _interopRequireDefault(_sf);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	//resolved in webpack's "externals"
+	
+	var Input = function Input(sf, node, options) {
+	    this._construct(sf, node, options);
+	};
+	
+	/**
+	 * @lends sf.Form.prototype
+	 */
+	Input.prototype = (0, _create2.default)(_sf2.default.modules.core.BaseDOMConstructor.prototype);
+	
+	/**
+	 * Name to register
+	 * @type {string}
+	 */
+	Input.prototype.name = "input";
+	
+	Input.prototype._construct = function (sf, node, options) {
+	
+	    this.init(sf, node, options); //call parent
+	
+	    if (options) {
+	        //if we pass options extend all options by passed options
+	        this.options = sf.tools.extend(this.options, options);
+	    }
+	
+	    node.value = this.options.prefix + " " + node.value;
+	
+	    //elements
+	    this.els = {
+	        node: node
+	    };
+	
+	    this.addEventListeners();
+	};
+	
+	/**
+	 * @override
+	 * @inheritDoc
+	 * @enum {string}
+	 */
+	Input.prototype.optionsToGrab = {
+	    /**
+	     *  Mask of input
+	     */
+	    "mask": {
+	        "domAttr": "data-mask"
+	    },
+	    /**
+	     *  Prefix of  input
+	     */
+	    "prefix": {
+	        "domAttr": "data-prefix"
+	    }
+	
+	};
+	
+	/**
+	 * Adds static events listeners.
+	 */
+	Input.prototype.addEventListeners = function () {
+	    var that = this;
+	
+	    this._inputKeyup = function () {
+	
+	        var oldValue = this.getAttribute('data-prefix');
+	
+	        if (that.els.node.value.indexOf(oldValue) !== 0) {
+	            that.els.node.value = oldValue + that.els.node.value;
+	        }
+	    };
+	
+	    if (this.els.node) {
+	        this.els.node.addEventListener('keyup', this._inputKeyup);
+	    }
+	};
+	
+	Input.prototype.removeEventListeners = function () {
+	    if (this.els.node) {
+	        this.els.node.removeEventListener('keyup', this._inputKeyup);
+	    }
+	};
+	
+	Input.prototype.die = function () {
+	    this.removeEventListeners();
+	    delete this;
+	};
+	
+	exports.default = Input;
+
+/***/ },
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	module.exports = { "default": __webpack_require__(4), __esModule: true };
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var $ = __webpack_require__(5);
+	module.exports = function create(P, D) {
+	  return $.create(P, D);
+	};
+
+/***/ },
+/* 5 */
+/***/ function(module, exports) {
+
+	"use strict";
+	
+	var $Object = Object;
+	module.exports = {
+	  create: $Object.create,
+	  getProto: $Object.getPrototypeOf,
+	  isEnum: {}.propertyIsEnumerable,
+	  getDesc: $Object.getOwnPropertyDescriptor,
+	  setDesc: $Object.defineProperty,
+	  setDescs: $Object.defineProperties,
+	  getKeys: $Object.keys,
+	  getNames: $Object.getOwnPropertyNames,
+	  getSymbols: $Object.getOwnPropertySymbols,
+	  each: [].forEach
+	};
+
+/***/ }
+/******/ ])
+});
+;
+//# sourceMappingURL=sf.input.js.map
---
+++ src/input/input.js
@@ -0,0 +1,95 @@
+"use strict";
+
+import sf from 'sf';//resolved in webpack's "externals"
+
+var Input = function (sf, node, options) {
+    this._construct(sf, node, options);
+};
+
+/**
+ * @lends sf.Form.prototype
+ */
+Input.prototype = Object.create(sf.modules.core.BaseDOMConstructor.prototype);
+
+/**
+ * Name to register
+ * @type {string}
+ */
+Input.prototype.name = "input";
+
+Input.prototype._construct = function (sf, node, options) {
+
+    this.init(sf, node, options);//call parent
+
+
+    if (options) {//if we pass options extend all options by passed options
+        this.options = sf.tools.extend(this.options, options);
+    }
+
+    node.value = this.options.prefix + " " + node.value;
+
+    //elements
+    this.els = {
+        node: node
+    };
+
+    this.addEventListeners();
+};
+
+
+/**
+ * @override
+ * @inheritDoc
+ * @enum {string}
+ */
+Input.prototype.optionsToGrab  = {
+    /**
+     *  Mask of input
+     */
+    "mask": {
+        "domAttr": "data-mask"
+    },
+    /**
+     *  Prefix of  input
+     */
+    "prefix": {
+        "domAttr": "data-prefix"
+    }
+
+};
+
+
+/**
+ * Adds static events listeners.
+ */
+Input.prototype.addEventListeners = function () {
+    var that = this;
+
+    this._inputKeyup = function(){
+
+        var oldValue = this.getAttribute('data-prefix');
+
+        if( that.els.node.value.indexOf(oldValue) !== 0 ){
+            that.els.node.value = oldValue + that.els.node.value;
+        }
+    };
+
+    if (this.els.node) {
+        this.els.node.addEventListener('keyup', this._inputKeyup);
+    }
+};
+
+Input.prototype.removeEventListeners = function () {
+    if (this.els.node) {
+        this.els.node.removeEventListener('keyup', this._inputKeyup);
+    }
+};
+
+Input.prototype.die = function () {
+    this.removeEventListeners();
+    delete this;
+};
+
+export { Input as default };
+
+
