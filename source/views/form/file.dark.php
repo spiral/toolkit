89fd7af6c5359d7cfe6dfff703a5fed13dc7ff52
Nataliya Zhukava <nataliya.zhukava@gmail.com>
2016-02-25 00:17:59 +0300

    add names

--- source/views/form/file.dark.php
+++ source/views/form/file.dark.php
@@ -1,17 +1,14 @@
 <extends:spiral:element/>
 
 <block:resources>
-
-        <resource:script href="resources/scripts/spiral/sf.input.js"/>
-
+    <resource:script href="resources/scripts/spiral/sf.input.js"/>
 </block:resources>
 
 <block:body>
-
     <div class="item-form ${wrapper-class}" node:attributes="prefix:wrapper">
-        <input type="file" id="${id}" class="inputfile ${class} sf-js-input" data-multiple-caption="{count} files selected" multiple />
+        <input type="file" id="${id}" class="inputfile ${class} sf-js-input" data-multiple-caption="{count} ${multiple | files selected}" multiple />
         <label for="${id}">
-            <span>${label}</span>
+            <span>${label | Choose file}</span>
         </label>
     </div>
 </block:body>
