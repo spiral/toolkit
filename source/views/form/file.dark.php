3f56179a8ab4ae4266c3f124b104ce89653160e8
Nataliya Zhukava <nataliya.zhukava@gmail.com>
2016-02-25 00:21:12 +0300

    class

--- source/views/form/file.dark.php
+++ source/views/form/file.dark.php
@@ -6,9 +6,9 @@
 
 <block:body>
     <div class="item-form ${wrapper-class}" node:attributes="prefix:wrapper">
-        <input type="file" id="${id}" class="inputfile ${class} sf-js-input" data-multiple-caption="{count} ${multiple | files selected}" multiple />
+        <input type="file" id="${id}" class="inputfile sf-js-input" data-multiple-caption="{count} ${multiple | files selected}" multiple />
         <label for="${id}">
-            <span>${label | Choose file}</span>
+            <span class="${class}">${label | Choose file}</span>
         </label>
     </div>
 </block:body>
