3c790315ef306341bac4bcc99acb688b62be3597
Nataliya Zhukava <nataliya.zhukava@gmail.com>
2016-02-25 00:33:57 +0300

    new classes for input

--- source/views/form/file.dark.php
+++ source/views/form/file.dark.php
@@ -5,10 +5,10 @@
 </block:resources>
 
 <block:body>
-    <div class="item-form ${wrapper-class}" node:attributes="prefix:wrapper">
+    <div class="file-field input-field" node:attributes="prefix:wrapper">
         <input type="file" id="${id}" class="inputfile sf-js-input" data-multiple-caption="{count} ${multiple | files selected}" multiple />
         <label for="${id}">
-            <span class="${class}">${label | Choose file}</span>
+            <span class="btn ${class}">${label | Choose file}</span>
         </label>
     </div>
 </block:body>
