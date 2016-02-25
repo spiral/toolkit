52475f995996bc3310aff0a62d33a6deee096d29
Nataliya Zhukava <nataliya.zhukava@gmail.com>
2016-02-25 23:24:31 +0300

    Add data file

--- source/views/form/file.dark.php
+++ source/views/form/file.dark.php
@@ -6,7 +6,7 @@
 
 <block:body>
     <div class="file-field input-field">
-        <input type="file" id="${id}" class="inputfile sf-js-file-input" data-multiple-caption="{count} ${multiple-text | files selected}" node:attributes/>
+        <input type="file" id="${id}" class="inputfile sf-js-file-input" data-file="true" data-multiple-caption="{count} ${multiple-text | files selected}" node:attributes/>
         <label for="${id}">
             <span class="btn ${class}">${label | Choose file}</span>
         </label>
