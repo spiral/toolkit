283d4344f024bebc02726a901793bdbd68536c62
Anton Titov <wolfy.jd@gmail.com>
2016-03-16 16:35:44 +0300

    Update cell.dark.php

--- source/views/grid/cell.dark.php
+++ source/views/grid/cell.dark.php
@@ -1,5 +1,11 @@
 <?php #compile
-$this->evaluatorVariable('__gridHeaders__[]', '<th data-sorter="${sorter}">${label}${title}${head}</th>');
+ob_start();
+?>
+    <block:cell.head>
+        <th data-sorter="${sorter}">${label}${title}${head}</th>
+    </block:cell.head>
+<?php #compile
+$__gridHeaders__[] = ob_get_clean();
 ob_start();
 ?>
     <td node:attributes>
