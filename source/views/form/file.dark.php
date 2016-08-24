90f394614166bea9613bf599e056fadc9ee0b5d6
Nataliya Zhukava <nataliya.zhukava@gmail.com>
2016-08-24 18:24:49 +0300

    M file input layout

--- source/views/form/file.dark.php
+++ source/views/form/file.dark.php
@@ -5,7 +5,7 @@
 </block:resources>
 
 <block:body>
-    <div class="file-field input-field">
+    <div class="file-field item-form">
         <input type="file" id="${id}" class="inputfile sf-js-file-input" data-file="true" data-multiple-caption="{count} ${multiple-text | files selected}" node:attributes/>
         <label for="${id}">
             <span class="btn ${class}">${label | Choose file}</span>
