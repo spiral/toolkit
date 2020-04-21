f7cff34ae0274e5f8aaec0417989f9b2df3ff31e
Andrew Kirkovski <andrew@spiralscout.com>
2020-04-21 09:47:36 +0300

    pattern input

--- packages/toolkit/src/index.js
+++ packages/toolkit/src/index.js
@@ -2,6 +2,7 @@ const core = require('@spiral-toolkit/core').default;
 
 require('./form/input');
 require('./ui/localdate/LocalDate');
+require('./form/pattern/PatternInput');
 
 require('@spiral-toolkit/autocomplete');
 require('@spiral-toolkit/locker');
