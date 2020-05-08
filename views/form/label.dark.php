169d68daa018327d61f14bc26a74e57aaa7c3885
Wolfy-J <wolfy.jd@gmail.com>
2020-05-08 14:06:39 +0300

    - labels can wrap checkbox groups

--- views/form/label.dark.php
+++ views/form/label.dark.php
@@ -1,4 +1,6 @@
 <div class="form-group col-${size|12}">
-  <label class="${class}" attr:aggregate>${context}${label}</label>
+  <label class="${class}" attr:aggregate>${label}</label>
   @if(injected('required'))<span class="text-danger">*</span>@endif
+  ${context}
+  <div data-message-placeholder="${name}"></div>
 </div>
