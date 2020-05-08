3913d96363695c409f3fd80bb6630e9be7737d1a
Wolfy-J <wolfy.jd@gmail.com>
2020-05-08 14:29:54 +0300

    - fix select value in radios

--- views/form/radio-group.dark.php
+++ views/form/radio-group.dark.php
@@ -6,7 +6,7 @@ $_selected_ = inject('value');
 ?>
 <div inputID:define class="form-group col-${size|12}" data-field="true">
   <div class="form-row">
-    @foreach($_values as $_val => $_lbl)
+    @foreach($_values_ as $_val => $_lbl)
       <div class="col col-auto">
         <div class="@if(injected('is-native'))form-check@else custom-control custom-radio@endif">
           <input
@@ -16,7 +16,7 @@ $_selected_ = inject('value');
             type="radio"
             name="${name}"
             value="{{ $_val }}"
-            @if($_val == $_selected)checked@endif
+            @if($_val == $_selected_)checked@endif
           >
           <label inputID:consume for="${id}_{{ $_val }}"
                  class="@if(injected('is-native'))form-check-label@else custom-control-label@endif">{{ $_lbl }}</label>
