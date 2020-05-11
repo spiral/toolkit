1316e83903eed8cf1e0add72c17601d58fba1630
Andrew Kirkovski <andrew@spiralscout.com>
2020-05-11 16:36:27 +0300

    datepicker styles moved to keeper

--- views/form/date-js.dark.php
+++ views/form/date-js.dark.php
@@ -1,19 +1,15 @@
 <extends:toolkit:form.field/>
 
 <block:element>
-  @if(injected('default-styles'))
-  <stack:push name="styles" unique-id="date-js-css">
-     <link rel="stylesheet" type="text/css" href="https://npmcdn.com/flatpickr/dist/themes/airbnb.css">
-  </stack:push>
-  @endif
   <div class="js-sf-date input-group"
        data-enable-time="${enable-time}"
+       @if(injected('time-24')) data-time-24="${time-24}" @endif
        data-no-calendar="${no-calendar}"
        data-format="${format}"
        data-display-format="${display-format}"
   >
       <div class="input-group-prepend" data-toggle>
-            <span class="form-control text-muted bg-white border-right-0">
+            <span class="form-control text-muted bg-white border-right-0" style="cursor: pointer">
                 <i class="fas fa-calendar"></i>
             </span>
       </div>
@@ -29,7 +25,7 @@
       >
       @if(injected('add-reset'))
           <div class="input-group-append" data-clear>
-                <span class="form-control text-muted bg-white border-left-0">
+                <span class="form-control text-muted bg-white border-left-0" style="cursor: pointer">
                     <i class="fas fa-times"></i>
                 </span>
           </div>
