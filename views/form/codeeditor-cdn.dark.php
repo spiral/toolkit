1a5e07b313b6b933688c5d5b81aeeb7a013df9af
Andrei Kirkouski <andrew@spiralscout.com>
2022-10-03 11:08:54 +0400

    add JS support by default

--- views/form/codeeditor-cdn.dark.php
+++ views/form/codeeditor-cdn.dark.php
@@ -1,5 +1,6 @@
 <stack:push name="scripts" unique-id="codemirror">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/codemirror.min.js" integrity="sha512-8RnEqURPUc5aqFEN04aQEiPlSAdE0jlFS/9iGgUyNtwFnSKCXhmB6ZTNl7LnDtDWKabJIASzXrzD0K+LYexU9g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.9/mode/javascript/javascript.js" integrity="sha512-9bZKsY+97AQthWWS54sUIWxo0GkS0yPvSXStcMshHDVj9lCfLgzhSTr9xa+Ho0WKkRTKMm2yy8M+EG10n9u0iQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </stack:push>
 <stack:push name="styles" unique-id="codemirror">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/codemirror.min.css" integrity="sha512-uf06llspW44/LZpHzHT6qBOIVODjWtv4MxCricRxkzvopAlSWnTf6hpZTFxuuZcuNE9CBQhqE0Seu1CoRk84nQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
